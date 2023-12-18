fc5aec51410f74846d5ecc19f72eadfb3d3ac8dd
Guilherme Ramos <105815487+GuilhermeRamosAju@users.noreply.github.com>
2023-12-18 11:23:02 -0300

    about(completed)

--- portifolio-publicitario/src/components/About.js
+++ portifolio-publicitario/src/components/About.js
@@ -3,16 +3,16 @@ import about from './images/about.png'
 const About = () =>{
   return(
     <section className={styles.about} id="about">
-      <img src={about} alt="Adrya usando o computador" />
-      <div>
-        <h2>Olá, sou Adrya.</h2>
-        <p>
+      <img className={styles.aboutImg} src={about} alt="Adrya usando o computador" />
+      <div className={styles.aboutTxt}>
+        <h2 className={styles.aboutH2}>Olá, sou Adrya.</h2>
+        <p className={styles.aboutP}>
           Uma jovem estudante de Comunicação Social - Publicidade e Propaganda. Com 21 anos de idade, sou movida por uma paixão intensa pela comunicação e todas as suas facetas.
         </p>
-        <p>
+        <p className={styles.aboutP}>
           Acredito que a comunicação seja a chave para expressão e conexão, tanto em âmbito pessoal quanto profissional e empresarial. Estou constantemente em busca de oportunidades para desenvolver meu conhecimento e habilidades nessa área fascinante.
         </p>
-        <p>
+        <p className={styles.aboutP}>
           Vamos desbloquear o seu sucesso juntos e criar conexões autenticas.
         </p>
       </div>
